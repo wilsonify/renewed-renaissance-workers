ccf1d5f1e1465bd8619210f7c2bb0d94d21c5259
wilsonify <tom.andrew.wilson@gmail.com>
2024-11-28 13:28:30 -0500

    pnpm-lock.yaml

--- hello/cool-feather-214b/test/index.spec.js
+++ hello/cool-feather-214b/test/index.spec.js
@@ -4,7 +4,7 @@ import worker from '../src';
 
 describe('Hello World worker', () => {
 	it('responds with Hello World! (unit style)', async () => {
-		const request = new Request('http://example.com');
+		const request = new Request('http://localhost:8787/');
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
@@ -13,8 +13,4 @@ describe('Hello World worker', () => {
 		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
 	});
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch(request, env, ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
 });
