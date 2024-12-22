443a2d929e962028fd9f32253451b48b1fb31ce3
wilsonify <tom.andrew.wilson@gmail.com>
2024-12-22 09:58:03 -0500

    bio.js

--- hello/my-gatsby-app/src/components/bio.js
+++ hello/my-gatsby-app/src/components/bio.js
@@ -48,9 +48,7 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
+
 
           <div>
             <p>You can Login here</p>
